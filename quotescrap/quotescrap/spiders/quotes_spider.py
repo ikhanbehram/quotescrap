2c4c7717b18bb5b08dd8800ea76c94fd0370352b
Behram <77900065+ikhanbehram@users.noreply.github.com>
2023-02-03 11:49:32 +0500

    data turned to items

--- quotescrap/quotescrap/spiders/quotes_spider.py
+++ quotescrap/quotescrap/spiders/quotes_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from ..items import QuotescrapItem
 
 class QuoteSpider(scrapy.Spider):
     name = 'quotes'
@@ -7,6 +8,21 @@ class QuoteSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        title = response.css('title::text').extract()
-        yield {'titleText': title}
+        items = QuotescrapItem()
 
+
+        divs = response.css('div.quote')
+        
+        for quote in divs:
+            title = quote.css('span.text::text').extract()
+            author = quote.css('.author::text').extract()
+            tag = quote.css('.tag::text').extract()
+            items['title'] = title
+            items['author'] = author
+            items['tag'] = tag
+
+            yield items
+        next_page = response.css('li.next a::attr(href)').get()
+        print(next_page)
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
